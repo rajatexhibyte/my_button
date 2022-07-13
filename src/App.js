722a986b2bebe6e4d916053a43fd28f1702eeb9c
reactexcel <rohit.eexcel2011@gmail.com>
2022-07-13 23:49:37 +0530

    changes in ui

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import MyButton from './components/button';
+
+function App() {
+  return (
+    <div style={{display:"flex", justifyContent: "center", marginTop: "50vh"}}>
+      <MyButton />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/button.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const MyButton = () => {
+  return (
+    <div>
+        <button style={{padding:"10px", background: "green", color: "white", borderRadius:"5px",border: "1px solid green", cursor: "pointer"}}>
+            Hey, I'm Button
+        </button>
+    </div>
+  )
+}
+
+export default MyButton
